ac7f6c1899db608730a9f8205607162744419615
Paulyn <paulyntran@live.ca>
2019-03-09 04:34:11 +0000

    Completed Quadratic vs linear sum activity

--- array-search.js
+++ array-search.js
@@ -13,26 +13,26 @@
 
 // Binary Search Function
 function search(array, item) {
-    let min = 0;
-    let max = array.length - 1; 
+    let min = 0; // 1
+    let max = array.length - 1; // 1
 
-    while (true) {
-        const middleIndex = Math.floor((min + max)/2.0);
-        const currentItem = array[middleIndex];
+    while (true) { // lg n
+        const middleIndex = Math.floor((min + max)/2.0); // lg n
+        const currentItem = array[middleIndex]; // lg n
 
-        if (currentItem === item) {
+        if (currentItem === item) { //lg n
             // We found it!
             return middleIndex;
-        } else if (currentItem < item) {
+        } else if (currentItem < item) { // lg n
             // Look at the right side
             min = middleIndex + 1;
         } else {
             // Look at the left side
-            max = middleIndex -1;
+            max = middleIndex -1; // lg n
         }
 
-        if (min > max) {
-            return null;
+        if (min > max) { //lg n
+            return null; //1 
         }
     }
 }
